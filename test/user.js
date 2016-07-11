45ad4f1aa17682a7af05c405169c08e525e59361
Lluis Agusit <lluiagusti@gmail.com>
2016-07-11 11:55:05 +0100

    Write test to assert `userFactory` returns an user with an API key

--- test/user.js
+++ test/user.js
@@ -1,2 +1,33 @@
-import 'babel-polyfill';
-import userFactory from '../lib/factories/user';
+'use strict';
+
+const expect = require('expect.js');
+const userFactory = require('../lib/factories/user');
+
+describe('Package User', () => {
+  let user;
+
+  beforeEach(function() {
+    user = userFactory();
+  });
+
+  afterEach(function() {
+    user = undefined;
+  });
+
+  it('Should exist', () => {
+    expect(user).to.be.a('object');
+  });
+
+  it('Should have an API key', () => {
+    expect(user).to.have.key('key');
+  });
+
+  // it('Should have a location', () => {
+  //   expect(user).to.have.key('location');
+  // });
+
+  // it('Should have an activity', () => {
+  //   expect(user).to.have.key('activity');
+  // });
+});
+

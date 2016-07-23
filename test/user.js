47b725d221b2552eff2d3d9178d8f49141c6eff3
Lluis Agusit <lluiagusti@gmail.com>
2016-07-23 22:11:48 +0100

    Add basic implementation of User factory + tests
    - it only has an API key prop for now
    - it has a getter and setter for its API key

--- test/user.js
+++ test/user.js
@@ -1,22 +1,28 @@
 'use strict';
 
 const expect = require('expect.js');
+const user = require('../lib/factories/user');
 
 describe('Package User', () => {
-  const user = require('../lib/factories/user');
-
   it('Should exist', () => {
-    expect(user).to.be.a('object');
+    let u = user('abc'); 
+    expect(u).to.be.a('object');
   });
 
-  it('Should have a default API key', () => {
-    expect(user.getAPIkey()).to.be.a('string');
+  it('Should have a way to get its API key', () => {
+    let u = user('abc');
+    expect(u).to.have.key('getAPIkey');
   });
 
-  it('Should be able to set the API key', () => {
-    let key = 'test';
-    user.setAPIkey(key);
-    expect(user.getAPIkey()).to.be(key);
+  it('Should have an API key', () => {
+    let u = user('abc');
+    expect(u.getAPIkey()).to.be('abc');
+  });
+
+  it('Should be able to set an API key', () => {
+    let u = user('abc');
+    u.setAPIkey('def');
+    expect(u.getAPIkey()).to.be('def');
   });
 });
 
